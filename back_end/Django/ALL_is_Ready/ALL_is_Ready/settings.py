adb684f2906b88add150e418327b96034f747d60
李晟迪 <68073069+lishengdi@users.noreply.github.com>
2022-03-25 10:58:16 +0800

    Django Restful Framework apply
    Django Restful Framework apply

--- back_end/Django/ALL_is_Ready/ALL_is_Ready/settings.py
+++ back_end/Django/ALL_is_Ready/ALL_is_Ready/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'UserProfile',
-    'models'
+    'models',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- back_end/Django/ALL_is_Ready/models/models.py
+++ back_end/Django/ALL_is_Ready/models/models.py
@@ -7,11 +7,11 @@ class User(models.Model):
         verbose_name = '用户'
         verbose_name_plural = '用户'
 
-    UID = models.AutoField("用户ID", primary_key=True)
-    Uname = models.CharField("用户名", max_length=25, default='unnamed')
-    Passwd=models.CharField("密码",max_length=25,default='password-not-set')
-    OrgID=models.IntegerField("班级ID")
-    STUID=models.IntegerField("学号")
-    Rank=models.IntegerField("等级")
-    Header=models.ImageField("用户头像",upload_to='userHeader',default='/static/img/header_default.png')
-    EXP=models.IntegerField("用户经验值")
+    UID = models.AutoField(verbose_name="用户ID", primary_key=True)
+    Uname = models.CharField(verbose_name="用户名", max_length=25, default='unnamed')
+    Passwd=models.CharField(verbose_name="密码",max_length=25,default='password-not-set')
+    OrgID=models.IntegerField(verbose_name="班级ID")
+    STUID=models.IntegerField(verbose_name="学号")
+    Rank=models.IntegerField(verbose_name="等级")
+    Header=models.ImageField(verbose_name="用户头像",upload_to='userHeader',default='/static/img/header_default.png')
+    EXP=models.IntegerField(verbose_name="用户经验值")
